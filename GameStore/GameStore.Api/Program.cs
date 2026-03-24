fdce3d0893d0bddae45be686a8f109098d9e0643
Piero Aguilar Anticona <107908190+Piers18@users.noreply.github.com>
2026-03-24 09:07:07 -0500

    Add Game and Genre models with DbContext for GameStore API
    - Created Game and Genre models with necessary properties.
    - Implemented GameStoreContext inheriting from DbContext to manage database operations.
    - Updated project.nuget.cache with new package dependencies.

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,11 +1,16 @@
 using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
+using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connectionString = "Server=localhost;Database=GameStoreDb;User Id=sa;Password=Krypton#4826!;TrustServerCertificate=True";
+builder.Services.AddDbContext<GameStoreContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
